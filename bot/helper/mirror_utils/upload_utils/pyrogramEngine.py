0dcd83c8e602ebdbf193783459e706aa62b2c395
amaldezuza <56785114+amaldezuza@users.noreply.github.com>
2021-12-17 17:28:05 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -34,7 +34,7 @@ class TgUploader:
         self.message_id = listener.uid
         self.user_id = listener.message.from_user.id
         self.as_doc = AS_DOCUMENT
-        self.thumb = "/app/bot/thumb.jpg"
+        self.thumb = "/MirLeechX/bot/thumb.jpg"
         self.sent_msg = self.__app.get_messages(self.chat_id, self.message_id)
 
     def upload(self):
